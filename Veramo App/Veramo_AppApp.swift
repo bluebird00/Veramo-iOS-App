ca5b3fe157c23e57436108b89e696b4cdb4831bc
Rainer Schanung <rainerschanung@gmail.com>
2025-12-06 10:08:00 -0800

    Initial Commit

---
+++ Veramo App/Veramo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Veramo_AppApp.swift
+//  Veramo App
+//
+//  Created by rentamac on 12/6/25.
+//
+
+import SwiftUI
+
+@main
+struct Veramo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
